8743c384dc0435a66cda081e85930447742a2747
IlyaKharabet <ilyakharabet@gmail.com>
2017-03-25 22:37:39 +0300

    Setup project

---
+++ Foto/Foto/Foto.h
@@ -0,0 +1,19 @@
+//
+//  Foto.h
+//  Foto
+//
+//  Created by Ilya Kharabet on 25.03.17.
+//  Copyright © 2017 Mesterra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Foto.
+FOUNDATION_EXPORT double FotoVersionNumber;
+
+//! Project version string for Foto.
+FOUNDATION_EXPORT const unsigned char FotoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Foto/PublicHeader.h>
+
+
